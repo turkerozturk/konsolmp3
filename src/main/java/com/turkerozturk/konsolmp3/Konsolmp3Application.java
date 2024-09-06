c121947ee48c6ae78c9dd76b70520103b0ed345f
turkerozturk <turker_ozt@yahoo.com>
2024-09-06 16:55:40 +0300

    initializr: GraalVM Native Support, Spring Shell

---
+++ src/main/java/com/turkerozturk/konsolmp3/Konsolmp3Application.java
@@ -0,0 +1,13 @@
+package com.turkerozturk.konsolmp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Konsolmp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Konsolmp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/turkerozturk/konsolmp3/Konsolmp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkerozturk.konsolmp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Konsolmp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
